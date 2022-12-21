673d63e81d5b9155564434415795e00c5419749a
Istvan <olivaistvan@gmail.com>
2022-12-21 16:54:55 -0300

    feat: implements is_anagram()

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,31 @@
-def is_anagram(first_string, second_string):  # initial commit
-    """Faça o código aqui."""
-    raise NotImplementedError
+def turn_into_dict(string):
+    letters = dict()
+    for character in string.lower():
+        if character in letters:
+            letters[character] += 1
+        else:
+            letters[character] = 1
+
+    return letters
+
+
+def turn_into_word(dict):
+    sorted_word = ""
+    while len(dict) > 0:
+        letter = min(dict)
+        for item in range(dict[letter]):
+            sorted_word += letter
+        del dict[f"{letter}"]
+
+    return sorted_word
+
+
+def is_anagram(first_string, second_string):
+    first_dict = turn_into_dict(first_string.lower())
+    second_dict = turn_into_dict(second_string.lower())
+
+    first = turn_into_word(first_dict)
+    second = turn_into_word(second_dict)
+    comparison = first == second and first != ''
+
+    return first, second, comparison
