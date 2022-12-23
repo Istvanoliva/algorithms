8995bde97ce166990e957192a859f11ee61920db
Istvan <olivaistvan@gmail.com>
2022-12-22 23:33:37 -0300

    feat: implements find_duplicate()

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
+from collections import Counter
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if nums:
+        for item in nums:
+            if type(item) != int or item < 1:
+                return False
+        list_nums = Counter(nums).most_common(1)[0]
+        if list_nums[1] > 1:
+            return list_nums[0]
+    return False
